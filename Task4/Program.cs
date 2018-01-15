95635f25983b8ea5b99651efe0e4f0a545998552
Reinger <dvbolotov@edu.hse.ru>
2018-01-15 10:10:59 +0500

    Ввод значение e

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -8,8 +8,27 @@ namespace Task4
 {
     class Program
     {
+        static double ReadDouble()
+        {
+            double k;
+            bool ok;
+            do
+            {
+                Console.Write("Введите е: ");
+                ok = Double.TryParse(Console.ReadLine(), out k);
+                if (!ok)
+                {
+                    Console.WriteLine("Ошибка ввода");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            } while (!ok);
+            return k;
+        }
+
         static void Main(string[] args)
         {
+            double e = ReadDouble();
         }
     }
 }
